a62fa886a832685f8fe0dbe79bcc2b317652d261
jensen-dong <jensendong141@gmail.com>
2024-06-26 20:31:07 -0700

    feat: add handle submit logic and finish lab

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -13,37 +13,48 @@ const Bookshelf = () => {
 
   const handleInputChange = (event) => {
     setNewBook({ ...newBook, [event.target.name]: event.target.value });
-    
-  }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setBooks([...books, newBook]);
+    setNewBook({ title: "", author: "" });
+  };
 
   return (
     <div className="bookshelfDiv">
       <div className="formDiv">
         <h3>Add a Book</h3>
-        <form>
-        <div>
-          <label htmlFor="title">Title: </label>
-          <input
-            id="title"
-            name="title"
-            value={newBook.title}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="author">Author: </label>
-          <input
-            id="author"
-            name="author"
-            value={newBook.author}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit">Submit</button>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Title: </label>
+            <input
+              id="title"
+              name="title"
+              value={newBook.title}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="author">Author: </label>
+            <input
+              id="author"
+              name="author"
+              value={newBook.author}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
         </form>
       </div>
-      <div className="bookCardsDiv">{/* Book cards will display here */}</div>
-      
+      <div className="bookCardsDiv">
+        {books.map((book) => (
+          <div key={book.title} className="bookCard">
+            <h3>{book.title}</h3>
+            <p>By {book.author}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
